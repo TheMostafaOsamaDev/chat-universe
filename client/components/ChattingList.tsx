2d9b3531634337e5cbab36637b89572dce2008ef
MostafaOsama <mostafaos2021fp@gmail.com>
2024-11-27 11:09:27 +0200

    Add ChatBubble component and update ChattingList to display messages

--- client/components/ChattingList.tsx
+++ client/components/ChattingList.tsx
@@ -1,6 +1,13 @@
 "use client";
 import React from "react";
+import ChatBubble from "./ChatBubble";
 
 export default function ChattingList() {
-  return <div className="flex-1">ChattingList</div>;
+  return (
+    <div className="flex-1 bg-red-600 max-h-[75vh] overflow-y-auto">
+      {Array.from({ length: 50 }).map((_, i) => (
+        <ChatBubble key={i} message={`Message ${i}`} />
+      ))}
+    </div>
+  );
 }
